a0fac4659ac7f0705c7b8e709e7422b33b78d3ed
jader.belarmino <jader.belarmino@tst.jus.br>
2016-06-29 14:49:59 -0300

    conectando sem proxy
    testar fora do TST

--- src/main/java/br/jus/tst/TokenServlet.java
+++ src/main/java/br/jus/tst/TokenServlet.java
@@ -3,22 +3,16 @@ package br.jus.tst;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.URI;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.NTCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.jboss.resteasy.client.ClientExecutor;
-import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.ClientRequestFactory;
-import org.jboss.resteasy.client.core.executors.ApacheHttpClientExecutor;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.google.gson.Gson;
 
@@ -37,25 +31,40 @@ public class TokenServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		response.sendRedirect("http://revistadostribunais.com.br/maf/app/authentication/signon?sso-token=" + getToken());
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.sendRedirect("http://revistadostribunais.com.br/maf/app/authentication/signon?sso-token="
+				+ getToken());
 	}
 
 	public static String getToken() {
 		String tokenResultado = "";
 		// Definindo Usu�rio e Senha
-		
+
 		try {
-			HttpClient httpclient = new HttpClient();
-			ClientExecutor clientExecutor = new ApacheHttpClientExecutor(httpclient);
-			ClientRequestFactory factory = new ClientRequestFactory(clientExecutor, new URI(""));
-			ClientRequest request = factory
-					.createRequest("http://revistadostribunais.com.br/maf/api/v1/authenticate.json?sp=[USUARIO]");
+
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(
+					"http://revistadostribunais.com.br/maf/api/v1/authenticate.json?sp=TST-3");
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			String resultado = "";
+			while ((resultado = rd.readLine()) != null) {
+				System.out.println(resultado);
+			}
+
+			// HttpClient httpclient = new HttpClient();
+			// ClientExecutor clientExecutor = new
+			// ApacheHttpClientExecutor(httpclient);
+			// ClientRequestFactory factory = new
+			// ClientRequestFactory(clientExecutor, new URI(""));
+			// ClientRequest request = factory
+			// .createRequest("http://revistadostribunais.com.br/maf/api/v1/authenticate.json?sp=[USUARIO]");
 
 			// obtendo o resultado do WebService via GET
-			String resultado = request.getTarget(String.class);
+			// String resultado = request.getTarget(String.class);
 
 			Gson gson = new Gson();
 			Token t = gson.fromJson(resultado, Token.class);
@@ -64,7 +73,7 @@ public class TokenServlet extends HttpServlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return tokenResultado;
 
 	}
